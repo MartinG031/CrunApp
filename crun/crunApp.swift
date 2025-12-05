b75bbf214514c093a8f1dc281d717ae70ee0259f
MartinJay <martinjay200031@gmail.com>
2025-12-05 15:47:26 +0800

    Initial Commit

---
+++ crun/crunApp.swift
@@ -0,0 +1,17 @@
+//
+//  crunApp.swift
+//  crun
+//
+//  Created by Martin Jay on 2025/12/5.
+//
+
+import SwiftUI
+
+@main
+struct crunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
